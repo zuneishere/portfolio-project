e3532213ba5097c1cad0f0b9aefbd6a4a721a57a
zuneishere <zuneishere@gmail.com>
2018-11-27 12:44:10 +0530

    added local settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,3 +133,9 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
